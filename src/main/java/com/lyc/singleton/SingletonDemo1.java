d47882e18ec11433f86f046f14d386caf0ead790
littlelittlemonkey <1575571143@qq.com>
2018-10-28 09:54:01 +0800

    单例模式：饿汉式-线程安全

---
+++ src/main/java/com/lyc/singleton/SingletonDemo1.java
@@ -0,0 +1,19 @@
+package com.lyc.singleton;
+
+/**
+ * 测试饿汉式单例模式
+ * 缺点类加载时就创建对象，可能并没有用到，浪费内存
+ */
+public class SingletonDemo1 {
+
+    //类初始化时，立即加载这个对象（没有延时加载的优势）。加载类时，天然的是线程安全的！
+    private static SingletonDemo1 instance = new SingletonDemo1();
+
+    private SingletonDemo1(){
+    }
+
+    //方法没有同步，调用效率高！
+    public static SingletonDemo1  getInstance(){
+        return instance;
+    }
+}
