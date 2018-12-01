79407d075b185a957eb11d2731fc0f38d73c6af4
littlelittlemonkey <1575571143@qq.com>
2018-12-01 11:51:34 +0800

    建造者模式

---
+++ src/main/java/com/lyc/builder/AirShipBuilder.java
@@ -0,0 +1,8 @@
+package com.lyc.builder;
+
+
+public interface AirShipBuilder {
+	Engine builderEngine();
+	OrbitalModule builderOrbitalModule();
+	EscapeTower  builderEscapeTower();
+}
---
+++ src/main/java/com/lyc/builder/AirShipDirector.java
@@ -0,0 +1,11 @@
+package com.lyc.builder;
+
+public interface AirShipDirector {
+	
+	/**
+	 * 组装飞船对象
+	 * @return
+	 */
+	AirShip   directAirShip();
+	
+}
